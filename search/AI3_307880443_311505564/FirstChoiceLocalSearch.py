e4160bad4c92df3b8fdd98b13b4425a5d2b5402f
eyalt <eyaltolchinsky@gmail.com>
2015-01-20 12:15:22 +0200

    there might be a problem with the suffle in use (such as in create_ops)

--- search/AI3_307880443_311505564/FirstChoiceLocalSearch.py
+++ search/AI3_307880443_311505564/FirstChoiceLocalSearch.py
@@ -1,24 +1,46 @@
 '''
-Created on 19 αιπε 2015
-
 @author: Eyal
 '''
 
 from abstract_search import LocalSearch
 from random import shuffle
+from data import load_hw3_data_1
+from classifier import KNearestNeighbours
+from search.AI3_307880443_311505564.LearningState import create_learning_state_ops, \
+    LearningState
 
 class FirstChoiceLocalSearch(LocalSearch):
     def search(self, evaluation_set, evaluation_set_labels, *args, **kwargs):
         current = self._current_state
         current_evaluation = current.evaluate(evaluation_set, evaluation_set_labels, *args, **kwargs)
         
+        depth = 0
         while True:
+            depth += 1
+            print depth
             for next_state, next_op in shuffle(current.get_next_states()):
                 next_evaluation = next_state.evaluate(evaluation_set, evaluation_set_labels, *args, **kwargs)
                 if next_evaluation > current_evaluation:
                     current = next_state
                     current_evaluation = next_evaluation
+                    evaluation_set = next_op.remove_attributes(evaluation_set)
                     self.operators.append(next_op)
                     break
             else:
+                print "Finished searching after %d steps" % (depth)
                 return current, self.operators
+
+
+if __name__ == "__main__":
+    training, validation, test = load_hw3_data_1()
+    training_set, training_labels = training
+    validation_set, validation_labels = validation
+    test_set, test_labels = test
+    classifier = KNearestNeighbours(3)
+    
+    startingState = LearningState(create_learning_state_ops(training_set), training_set, training_labels)
+    localSearch = FirstChoiceLocalSearch(startingState)
+    
+    state, ops = localSearch.search(validation_set, validation_labels, classifier)
+    print "State:", state
+    print "Ops:", ops
--- search/AI3_307880443_311505564/LearningState.py
+++ search/AI3_307880443_311505564/LearningState.py
@@ -1,9 +1,10 @@
 '''
-Created on 19 αιπε 2015
-
 @author: Eyal
 '''
+
 from abstract_search import SearchState
+import copy
+from random import shuffle
 
 class LearningState(SearchState):
     def __init__(self, legal_operators, training_set, training_labels):
@@ -21,12 +22,56 @@ def calc_accuracy(classified_labels, real_labels):
     return float(len([i for i in range(total_tests) if classified_labels[i] == real_labels[i]])) / total_tests
 
 class LearningStateOperator:
-    def __init__(self,rows_to_remove,cols_to_remove):
+    def __init__(self, rows_to_remove=[], cols_to_remove=[]):
+        assert len(rows_to_remove) == 0 or len(cols_to_remove) == 0
         self._rows_to_remove = rows_to_remove
         self._cols_to_remove = cols_to_remove
         
     def __call__(self, learningState):
-        ops = create_learning_state_ops(learningState.training_set)
+        new_training_set, new_training_labels = self._update_training(learningState.training_set, learningState.training_labels)
+            
+        # now the training set and training labels have been updated
+        # build the new operations for the new state
+        ops = create_learning_state_ops(new_training_set)
         
-def create_learning_state_ops(subjects_set):
-    pass
+        return LearningState(ops, new_training_set, new_training_labels)
+        
+    def remove_attributes(self, example_set):
+        assert len(self._cols_to_remove) == 0 or len(self._cols_to_remove) == 1
+        
+        new_example_set = copy.deepcopy(example_set)
+        
+        for i in sorted(self._cols_to_remove, reverse=True):
+            for example in new_example_set:
+                example.pop(i)
+        return new_example_set
+    
+    def _update_training(self, training_set, training_labels):
+        '''removes the examples from both the set and the labels, and removes the attributes
+        returns a copy of the lists'''
+        assert len(self._rows_to_remove) == 0 or len(set(self._rows_to_remove)) == 5 or len(training_set) <= 5
+        assert len(self._cols_to_remove) == 0 or len(self._cols_to_remove) == 1
+        assert len(self._rows_to_remove) == 0 or len(self._cols_to_remove) == 0
+        
+        # first copy the set and labels, while removing unnecessary attributes
+        new_training_set = self.remove_attributes(training_set)
+        new_training_labels = copy.deepcopy(training_labels)
+        
+        # remove unnecessary examples
+        for i in sorted(self._rows_to_remove, reverse=True):
+            new_training_set.pop(i)
+            new_training_labels.pop(i)
+        return new_training_set, new_training_labels
+        
+def create_learning_state_ops(example_set):
+    num_atts = len(example_set[0])
+    rows_indexes = range(len(example_set))
+    print "Creating learning state ops"
+    print "#examples: %d, #atts: %d" % (num_atts, len(example_set))
+    
+    ops = [LearningStateOperator(cols_to_remove=[i]) for i in range(num_atts)]
+    for i in range(num_atts):
+        ops.append(LearningStateOperator(rows_to_remove=(shuffle(rows_indexes))[:5]))
+    
+    assert len(ops) == 2 * num_atts
+    return ops
